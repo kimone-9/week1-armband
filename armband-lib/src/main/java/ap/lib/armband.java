364467619eb304c338c05b4c550923d8b7313f79
1403541 <1403541@labcws-23.STUDENTS.utech.edu.jm>
2019-01-14 16:23:57 -0500

    other commits

--- armband-lib/src/main/java/ap/lib/armband.java
+++ armband-lib/src/main/java/ap/lib/armband.java
@@ -2,4 +2,25 @@ package ap.lib;
 
 public class armband {
 
+	private int id;
+	private String colour;
+	private float price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
 }
